eecfd93ae65db3a7158e671789c8f654ea305ac0
Younghee Rhee <rheey90@gmail.com>
2019-11-13 15:01:24 +0900

    Established server on local

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const parser = require("body-parser");
+const app = express();
+const port = 3001;
+
+app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
+app.use(parser.json());
+
+app.set("port", port);
+
+app.listen(port);
+console.log("Listening on:", port);
+
+module.exports.app = app;
