ad434741244ad3333873ea66d62aa438916d0b6a
Younghee Rhee <rheey90@gmail.com>
2019-11-14 00:45:42 +0900

    Added middlewares

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const parser = require("body-parser");
+const router = require("./routes.js");
+const jwtConfig = require("./config/jwtConfig");
 const app = express();
 const port = 3001;
 
 app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
 app.use(parser.json());
+app.use("/", router);
 
+app.set("jwt-secret", jwtConfig.secret);
 app.set("port", port);
 
 app.listen(port);
