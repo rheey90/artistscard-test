da3d2b0b80efcf44025bf0cc8d9f6f5cc070d6ec
Younghee Rhee <rheey90@gmail.com>
2019-11-14 22:19:35 +0900

    Initial commit for client

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import WrappedLogin from "./pages/Login";
+import WrappedSignUp from "./pages/SignUp";
+
+class App extends React.Component {
+  state = { currentUser: "" };
+  setCurrentUser = username => {
+    this.setState({ currentUser: username });
+  };
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => (
+              <WrappedLogin
+                currentUser={this.state.currentUser}
+                setCurrentUser={this.setCurrentUser}
+              />
+            )}
+          />
+          <Route path="/signup" component={WrappedSignUp} />
+          <Route
+            path="/page"
+            component={() => (
+              <Page
+                currentUser={this.state.currentUser}
+                setLogout={this.setCurrentUser}
+              />
+            )}
+          />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,4 @@
+/* eslint-disable react/prop-types */
+
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
