afe2a5aedb281259f79b9410d3e380afff4ebd9f
Younghee Rhee <rheey90@gmail.com>
2019-11-14 18:43:44 +0900

    Complete

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,6 @@
 const controller = require("./controller");
-const auth = require("./middleware/auth");
+const uploadmp3 = require("./s3/file-upload-mp3");
 const router = require("express").Router();
-
-router.post("/sign/checkid", controller.user.checkid);
 
 router.post("/sign/signup", controller.user.signup);
 
@@ -11,5 +9,13 @@ router.post("/sign/signin", controller.user.signin);
 router.get("/sign/signout", controller.user.signout);
 
 router.put("/sign/:username/changepw", controller.user.changepw);
+
+router.post("/music/:username/uploadtos3", uploadmp3.single("mp3"), (req, res) => {
+  return res.json({ mp3Url: req.file.location });
+});
+
+router.post("/music/:username", controller.music.insertdata);
+
+router.put("/music/:username/:dataid", controller.music.editdata);
 
 module.exports = router;
